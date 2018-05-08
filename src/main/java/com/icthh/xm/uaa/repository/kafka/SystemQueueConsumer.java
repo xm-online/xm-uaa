5113ff9a88f97164de278df591c9755cf78b1098
emonastyrev <emonastyrev@jbs.com.ua>
2018-05-08 15:07:40 +0300

    add event listeners

--- src/main/java/com/icthh/xm/uaa/repository/kafka/SystemQueueConsumer.java
+++ src/main/java/com/icthh/xm/uaa/repository/kafka/SystemQueueConsumer.java
@@ -6,6 +6,8 @@ import static com.icthh.xm.commons.lep.XmLepConstants.THREAD_CONTEXT_KEY_TENANT_
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.icthh.xm.commons.config.client.config.InitRefreshableConfigurationBeanPostProcessor;
+import com.icthh.xm.commons.messaging.event.system.SystemEventType;
 import com.icthh.xm.lep.api.LepManager;
 import com.icthh.xm.commons.logging.aop.IgnoreLogginAspect;
 import com.icthh.xm.commons.logging.util.MdcUtils;
@@ -26,6 +28,7 @@ import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -61,18 +64,7 @@ public class SystemQueueConsumer {
 
                 switch (event.getEventType().toUpperCase()) {
                     case Constants.UPDATE_ACCOUNT_EVENT_TYPE:
-                        String userKey = String.valueOf(event.getDataMap().get(Constants.SYSTEM_EVENT_PROP_USER_KEY));
-
-                        init(event.getTenantKey(), event.getUserLogin());
-
-                        log.info("Start to update account for userKey='{}'", userKey);
-                        User user = userService.getUser(userKey);
-                        if (user == null) {
-                            log.error("Failed to update account. User with userKey='{}' does not exists.", userKey);
-                        } else {
-                            SystemEventMapper.toUser(event, user);
-                            userService.saveUser(user);
-                        }
+                        onUpdateAcount(event);
                         break;
                     default:
                         log.info("Event ignored with type='{}', source='{}', event_id='{}'",
@@ -109,4 +101,18 @@ public class SystemQueueConsumer {
         MdcUtils.removeRid();
     }
 
+    private void onUpdateAcount(SystemEvent event) {
+        String userKey = String.valueOf(event.getDataMap().get(Constants.SYSTEM_EVENT_PROP_USER_KEY));
+
+        init(event.getTenantKey(), event.getUserLogin());
+
+        log.info("Start to update account for userKey='{}'", userKey);
+        User user = userService.getUser(userKey);
+        if (user == null) {
+            log.error("Failed to update account. User with userKey='{}' does not exists.", userKey);
+        } else {
+            SystemEventMapper.toUser(event, user);
+            userService.saveUser(user);
+        }
+    }
 }
--- src/main/java/com/icthh/xm/uaa/repository/kafka/SystemTopicConsumer.java
+++ src/main/java/com/icthh/xm/uaa/repository/kafka/SystemTopicConsumer.java
@@ -1,17 +1,30 @@
 package com.icthh.xm.uaa.repository.kafka;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.icthh.xm.commons.config.client.config.InitRefreshableConfigurationBeanPostProcessor;
 import com.icthh.xm.commons.logging.util.MdcUtils;
+import com.icthh.xm.commons.messaging.event.system.SystemEventType;
+import com.icthh.xm.uaa.config.Constants;
+import com.icthh.xm.uaa.domain.kafka.SystemEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SystemTopicConsumer {
+
+    private final InitRefreshableConfigurationBeanPostProcessor postProcessor;
 
     /**
      * Consume tenant command event message.
@@ -24,9 +37,38 @@ public class SystemTopicConsumer {
         MdcUtils.putRid();
         try {
             log.info("Consume event from topic [{}]", message.topic());
+            ObjectMapper mapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            mapper.registerModule(new JavaTimeModule());
+            try {
+                SystemEvent event = mapper.readValue(message.value(), SystemEvent.class);
 
+                log.info("Process event from topic [{}], type='{}', source='{}', event_id ='{}'",
+                    message.topic(), event.getEventType(), event.getMessageSource(), event.getEventId());
+
+                switch (event.getEventType().toUpperCase()) {
+                    case SystemEventType.SAVE_CONFIGURATION:
+                        onSaveConfiguration(event);
+                        break;
+                    default:
+                        log.info("Event ignored with type='{}', source='{}', event_id='{}'",
+                            event.getEventType(), event.getMessageSource(), event.getEventId());
+                        break;
+                }
+            } catch (IOException e) {
+                log.error("System queue message has incorrect format: '{}'", message.value(), e);
+            }
         } finally {
             MdcUtils.removeRid();
         }
+    }
+
+    private void onSaveConfiguration(SystemEvent event) {
+        String path = Objects.toString(event.getDataMap().get("path"), null);
+        if (StringUtils.isBlank(path)) {
+            throw new IllegalArgumentException("Event '" + event.getEventType() + "' configuration path can't be blank");
+        }
+        String hash = Objects.toString(event.getDataMap().get("hash"), null);
+        postProcessor.onConfigurationChanged(path, hash);
     }
 }
