faa42d6ffcd9a80523543c2cab2102a3fb2e6952
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-12-03 10:56:26 +0200

    dev

---
+++ src/main/java/com/icthh/xm/uaa/security/ldap/UaaBindAuthenticator.java
@@ -0,0 +1,25 @@
+package com.icthh.xm.uaa.security.ldap;
+
+
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.ldap.authentication.BindAuthenticator;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class UaaBindAuthenticator extends BindAuthenticator {
+
+    /**
+     * {@inheritDoc}
+     */
+    public UaaBindAuthenticator(BaseLdapPathContextSource contextSource) {
+        super(contextSource);
+    }
+
+    @Override
+    protected List<String> getUserDns(String username) {
+        LinkedList<String> parts = new LinkedList<>(Arrays.asList(username.split("@")));
+        return super.getUserDns(parts.getFirst());
+    }
+}
