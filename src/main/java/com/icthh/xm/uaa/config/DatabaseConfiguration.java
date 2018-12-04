c5a1aa7bc57f51d6e857c5f184fd498dae226cc7
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-12-04 15:45:45 +0200

    dev

--- src/main/java/com/icthh/xm/uaa/config/DatabaseConfiguration.java
+++ src/main/java/com/icthh/xm/uaa/config/DatabaseConfiguration.java
@@ -28,7 +28,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
