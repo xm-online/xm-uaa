c9560be8c7203a3b10dc23b6a489837296695f50
ssenko <sergeysenja1992@gmail.com>
2019-02-26 13:08:05 +0200

    Add cookies path

--- src/main/java/com/icthh/xm/uaa/web/rest/SocialController.java
+++ src/main/java/com/icthh/xm/uaa/web/rest/SocialController.java
@@ -67,7 +67,6 @@ public class SocialController {
                 request.getResponse().addHeader("X-ACTIVATION-CODE", answer.getActivationCode());
                 return redirect("/social-accept-connection");
             }
-
             return redirectOnError(providerId);
         } catch (Exception e) {
             return redirectOnError(providerId);
