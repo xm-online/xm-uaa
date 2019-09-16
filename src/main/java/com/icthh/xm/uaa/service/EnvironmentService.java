0b4f924c63590fe6e48b14710f53989d102df814
Anatoliy Medvedchuck <anatoliy.medvedchuk@jevera.software>
2019-09-16 16:54:01 +0300

    Fix Role matrix fail

--- src/main/java/com/icthh/xm/uaa/service/EnvironmentService.java
+++ src/main/java/com/icthh/xm/uaa/service/EnvironmentService.java
@@ -3,13 +3,15 @@ package com.icthh.xm.uaa.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.icthh.xm.commons.config.client.repository.TenantConfigRepository;
+import com.icthh.xm.commons.config.client.repository.CommonConfigRepository;
+import com.icthh.xm.commons.config.domain.Configuration;
 import com.icthh.xm.commons.permission.config.PermissionProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -20,17 +22,20 @@ public class EnvironmentService {
     private static final String NONE_TENANT = "-NONE-";
     private static final String API = "/api";
 
-    private final TenantConfigRepository configRepository;
+    private final CommonConfigRepository commonConfigRepository;
     private final PermissionProperties properties;
     private final AuthenticationService authenticationService;
-    private final TenantConfigRepository tenantConfigRepository;
 
     private final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 
     @SneakyThrows
     public List<String> getEnvironments() {
-        String environmentsFile = tenantConfigRepository.getConfigFullPath(null,
-            API + properties.getEnvSpecPath());
+        String environmentsFile = commonConfigRepository
+            .getConfig(null, Collections.singletonList(properties.getEnvSpecPath()))
+            .values().stream()
+            .map(Configuration::getContent)
+            .findFirst()
+            .orElse("---");
         return mapper.readValue(environmentsFile, new TypeReference<List<String>>() {
         });
     }
@@ -62,7 +67,10 @@ public class EnvironmentService {
     private boolean updateEnvironmentsConfig(String content) {
         String path = properties.getEnvSpecPath();
         try {
-            configRepository.updateConfigFullPath(null, API + path, content);
+            commonConfigRepository.updateConfigFullPath(Configuration.of()
+                                                                     .path(path)
+                                                                     .content(content)
+                                                                     .build(), null);
             return true;
         } catch (Exception e) {
             log.error("Error updating configuration {} for tenant {}",
