89dbf8329cc6fffbd1299adb092f5c6bd5b5997b
asychov <Freiberg25$Barcelona>
2020-07-16 18:07:28 +0300

    feature(allow password reset flow customization): allow password reset flow customization

--- src/main/java/com/icthh/xm/uaa/service/user/password/CustomPasswordResetHandler.java
+++ src/main/java/com/icthh/xm/uaa/service/user/password/CustomPasswordResetHandler.java
@@ -2,7 +2,6 @@ package com.icthh.xm.uaa.service.user.password;
 
 import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
-import com.icthh.xm.uaa.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +12,6 @@ public class CustomPasswordResetHandler implements PasswordResetHandler {
 
     @LogicExtensionPoint(value = "ResetPassword", resolver = CustomPasswordResetHandlerResolver.class)
     public void handle(PasswordResetRequest resetRequest) {
-
+        log.warn("No lep for type: {}", resetRequest.getResetType());
     }
 }
