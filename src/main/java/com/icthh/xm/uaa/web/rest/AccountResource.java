e89a7a7af8ab0c195d1f3d13a5e80c9c31b01edf
asychov <Freiberg25$Barcelona>
2020-07-23 13:06:38 +0300

    feature(allow password reset flow customization): allow password reset flow customization

--- src/main/java/com/icthh/xm/uaa/web/rest/AccountResource.java
+++ src/main/java/com/icthh/xm/uaa/web/rest/AccountResource.java
@@ -47,6 +47,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -84,6 +85,11 @@ public class AccountResource {
     private final TenantPermissionService tenantPermissionService;
     private final PasswordResetHandlerFactory resetFlowFactory;
     private final AccountMailService accountMailService;
+
+    @PostConstruct
+    private void init () {
+        log.info("Account resource inited");
+    }
 
     private String getRequiredUserKey() {
         return xmAuthenticationContextHolder.getContext().getRequiredUserKey();
