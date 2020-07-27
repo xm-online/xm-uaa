b4a9a723577f280de647b36a0d3156794e6652b2
asychov <Freiberg25$Barcelona>
2020-07-27 11:30:08 +0300

    change lep group

--- src/main/java/com/icthh/xm/uaa/service/account/password/reset/type/custom/CustomPasswordResetHandler.java
+++ src/main/java/com/icthh/xm/uaa/service/account/password/reset/type/custom/CustomPasswordResetHandler.java
@@ -7,13 +7,13 @@ import com.icthh.xm.uaa.service.account.password.reset.PasswordResetRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@LepService(group = "service.user.password.reset")
+@LepService(group = "service.user")
 @Service
 @Slf4j
 public class CustomPasswordResetHandler implements PasswordResetHandler {
 
     @Override
-    @LogicExtensionPoint(value = "ResetPassword", resolver = CustomPasswordResetHandlerResolver.class)
+    @LogicExtensionPoint(value = "SendPasswordResetKey", resolver = CustomPasswordResetHandlerResolver.class)
     public void handle(PasswordResetRequest resetRequest) {
         log.warn("No lep for type: {}", resetRequest.getResetType());
     }
