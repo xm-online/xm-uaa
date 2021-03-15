e95cd092c9ff918c7db116e0eaaae5e0461e0d68
Serhii Senko <serhii.senko@jevera.software>
2021-03-15 14:50:24 +0200

    Expose data spec and add data form

--- src/main/java/com/icthh/xm/uaa/web/rest/TenantPropertiesResource.java
+++ src/main/java/com/icthh/xm/uaa/web/rest/TenantPropertiesResource.java
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.icthh.xm.commons.permission.annotation.PrivilegeDescription;
+import com.icthh.xm.uaa.domain.UserSpec;
 import com.icthh.xm.uaa.domain.properties.TenantProperties;
 import com.icthh.xm.uaa.domain.properties.TenantProperties.PublicSettings;
 import com.icthh.xm.uaa.service.TenantPropertiesService;
 import com.icthh.xm.uaa.web.rest.vm.UaaValidationVM;
+import io.github.jhipster.web.util.ResponseUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -19,10 +21,13 @@ import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
@@ -76,5 +81,20 @@ public class TenantPropertiesResource {
     @Timed
     public PublicSettings getUaaPublicSettings() {
         return tenantPropertiesService.getTenantProps().getPublicSettings();
+    }
+
+    @GetMapping(value = "/uaa/properties/data-schema/{roleKey}")
+    @ApiOperation(value = "Get role data schema", response = UserSpec.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Uaa public settings", response = UserSpec.class),
+            @ApiResponse(code = 500, message = "Internal server error")})
+    @Timed
+    @PreAuthorize("hasPermission({'roleKey': #roleKey}, 'UAA.TENANT.PROPERTIES.UPDATE')")
+    @PrivilegeDescription("Privilege to get role data schema")
+    public UserSpec getRoleUserSpec(@PathVariable String roleKey) {
+        return tenantPropertiesService.getTenantProps().getUserSpec()
+                .stream()
+                .filter(it -> roleKey.equals(it.getRoleKey())).findFirst()
+                .orElse(new UserSpec(roleKey, null, null));
     }
 }
