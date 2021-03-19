a95f2529e3dfd2263b9df259b355718e2fdd0c50
olye0216 <oleksandr.yershov@netcracker.com>
2021-03-19 10:07:35 +0200

    fix for timeline-ignored-http-methods property

--- src/main/java/com/icthh/xm/uaa/config/ApplicationProperties.java
+++ src/main/java/com/icthh/xm/uaa/config/ApplicationProperties.java
@@ -2,6 +2,7 @@ package com.icthh.xm.uaa.config;
 
 import com.icthh.xm.commons.lep.TenantScriptStorage;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -27,6 +28,7 @@ public class ApplicationProperties {
     private final Lep lep = new Lep();
 
     private List<String> tenantIgnoredPathList;
+    private List<String> timelineIgnoredHttpMethods = Collections.emptyList();
     private List<String> proxyFilterWhiteList;
     private String kafkaSystemTopic;
     private String kafkaSystemQueue;
