602ee382581268ae7b4c67850b1dc74588c0b1c4
rternovyi <rternovyi@itsmartflex.com>
2021-11-24 16:58:24 +0200

    feat(config-retrieval): config retrieval exception handling
    Throw exception if config content could not be retrieved from config-app.

--- src/main/java/com/icthh/xm/uaa/service/TenantRoleService.java
+++ src/main/java/com/icthh/xm/uaa/service/TenantRoleService.java
@@ -514,7 +514,7 @@ public class TenantRoleService {
 
     private Optional<String> getConfigContent(String configPath) {
         String tenant = TenantContextUtils.getRequiredTenantKeyValue(tenantContextHolder.getContext());
-        String config = null;
+        String config;
         try {
             config = tenantConfigRepository.getConfigFullPath(tenant, API + configPath);
             if (StringUtils.isBlank(config) || EMPTY_YAML.equals(config)) {
@@ -522,7 +522,8 @@ public class TenantRoleService {
             }
 
         } catch (HttpClientErrorException e) {
-            log.warn("Error while getting '{}'", configPath, e);
+            log.warn("Error while getting '{" + configPath + "}'", e);
+            throw new IllegalStateException("Error while getting '{" + configPath + "}'", e);
         }
 
         return ofNullable(config);
