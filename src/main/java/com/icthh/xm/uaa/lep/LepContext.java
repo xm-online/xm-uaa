e049e35b760b3ba9bda1bbec726e21ec428c9aea
Serhii Senko <serhii.senko@jevera.software>
2022-02-23 09:55:49 +0200

    clean up code

--- src/main/java/com/icthh/xm/uaa/lep/LepContext.java
+++ src/main/java/com/icthh/xm/uaa/lep/LepContext.java
@@ -2,7 +2,6 @@ package com.icthh.xm.uaa.lep;
 
 import com.icthh.xm.commons.config.client.service.TenantConfigService;
 import com.icthh.xm.commons.lep.BaseProceedingLep;
-import com.icthh.xm.commons.lep.commons.CommonsService;
 import com.icthh.xm.commons.lep.spring.LepThreadHelper;
 import com.icthh.xm.commons.security.XmAuthenticationContext;
 import com.icthh.xm.commons.tenant.TenantContext;
@@ -17,28 +16,9 @@ import com.icthh.xm.uaa.service.TenantPropertiesService;
 import com.icthh.xm.uaa.service.UserLoginService;
 import com.icthh.xm.uaa.service.UserService;
 import com.icthh.xm.uaa.service.mail.MailService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_PROFILE_EVEBT_PRODUCER_SERVICE;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_ACCOUNT;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_CLIENT;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_CLIENT_DETAILS_SERVICE;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_CUSTOMIZABLE_TOKE_STORAGE;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_CUSTOM_AUTHORIZATION_CODE;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_LDAP_SERVICE;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_LEP_TOKEN_GRANTER;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_MAIL;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_TENANT_CONFIG_SERVICE;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_TENANT_PROPERTIES_SERVICE;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_USER;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_USER_DETAILS_SERVICE;
-import static com.icthh.xm.uaa.lep.XmUaaLepConstants.BINDING_SUB_KEY_SERVICE_USER_LOGIN_SERVICE;
 
 public class LepContext {
 
