50120a63d02a7f64572182d13bd3253329a24559
yekalinichenko <pkev777@gmail.com>
2022-02-23 19:19:13 +0200

    Disable hikari metrics

--- src/main/java/com/icthh/xm/uaa/config/AppMetricsConfiguration.java
+++ src/main/java/com/icthh/xm/uaa/config/AppMetricsConfiguration.java
@@ -12,39 +12,39 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
-@Configuration
-@EnableMetrics(proxyTargetClass = true)
-public class AppMetricsConfiguration extends MetricsConfigurerAdapter {
-
-    private static final String SCHEDULER = "scheduler";
-
-    private final MetricRegistry metricRegistry;
-    private final SchedulerMetricsSet schedulerMetricsSet;
-
-    private HikariDataSource hikariDataSource;
-
-    public AppMetricsConfiguration(MetricRegistry metricRegistry,
-                                   SchedulerMetricsSet schedulerMetricsSet) {
-
-        this.metricRegistry = metricRegistry;
-        this.schedulerMetricsSet = schedulerMetricsSet;
-    }
-
-    @Autowired(required = false)
-    public void setHikariDataSource(HikariDataSource hikariDataSource) {
-        this.hikariDataSource = hikariDataSource;
-    }
-
-    @PostConstruct
-    public void init() {
-        if (hikariDataSource != null) {
-            log.debug("Monitoring the datasource");
-            // remove the factory created by HikariDataSourceMetricsPostProcessor until JHipster migrate to Micrometer
-            hikariDataSource.setMetricsTrackerFactory(null);
-            hikariDataSource.setMetricRegistry(metricRegistry);
-        }
-
-        metricRegistry.register(SCHEDULER, schedulerMetricsSet);
-    }
-}
+//@Slf4j
+//@Configuration
+//@EnableMetrics(proxyTargetClass = true)
+//public class AppMetricsConfiguration extends MetricsConfigurerAdapter {
+//
+//    private static final String SCHEDULER = "scheduler";
+//
+//    private final MetricRegistry metricRegistry;
+//    private final SchedulerMetricsSet schedulerMetricsSet;
+//
+//    private HikariDataSource hikariDataSource;
+//
+//    public AppMetricsConfiguration(MetricRegistry metricRegistry,
+//                                   SchedulerMetricsSet schedulerMetricsSet) {
+//
+//        this.metricRegistry = metricRegistry;
+//        this.schedulerMetricsSet = schedulerMetricsSet;
+//    }
+//
+//    @Autowired(required = false)
+//    public void setHikariDataSource(HikariDataSource hikariDataSource) {
+//        this.hikariDataSource = hikariDataSource;
+//    }
+//
+//    @PostConstruct
+//    public void init() {
+//        if (hikariDataSource != null) {
+//            log.debug("Monitoring the datasource");
+//            // remove the factory created by HikariDataSourceMetricsPostProcessor until JHipster migrate to Micrometer
+//            hikariDataSource.setMetricsTrackerFactory(null);
+//            hikariDataSource.setMetricRegistry(metricRegistry);
+//        }
+//
+//        metricRegistry.register(SCHEDULER, schedulerMetricsSet);
+//    }
+//}
