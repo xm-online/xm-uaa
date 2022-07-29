4582dda4f58f515c9ad0e1665b04f8299bbc2837
aroze <aroze@vodafone.ua>
2022-07-29 16:03:31 +0300

    Fix Specification query count fetch

--- src/main/java/com/icthh/xm/uaa/service/query/UserQueryService.java
+++ src/main/java/com/icthh/xm/uaa/service/query/UserQueryService.java
@@ -1,7 +1,5 @@
 package com.icthh.xm.uaa.service.query;
 
-import static java.util.Optional.ofNullable;
-
 import com.icthh.xm.uaa.domain.User;
 import com.icthh.xm.uaa.domain.UserLogin;
 import com.icthh.xm.uaa.domain.UserLogin_;
@@ -11,25 +9,27 @@ import com.icthh.xm.uaa.service.dto.UserDTO;
 import com.icthh.xm.uaa.service.query.filter.SoftUserFilterQuery;
 import com.icthh.xm.uaa.service.query.filter.StrictUserFilterQuery;
 import io.github.jhipster.service.QueryService;
-import io.github.jhipster.service.filter.Filter;
 import io.github.jhipster.service.filter.StringFilter;
-
-import java.util.Collection;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Stream;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.Optional.ofNullable;
 
 @Service
 @RequiredArgsConstructor
@@ -57,9 +57,9 @@ public class UserQueryService extends QueryService<User> {
 
     private Specification<User> createSoftSpecification(SoftUserFilterQuery query) {
         return Stream.of(
-            ofNullable(query.getRoleKey()).map(fn -> buildStringSpecification(fn, User_.roleKey)),
-            ofNullable(query.getActivated()).map(fn -> buildSpecification(fn, User_.activated))
-        )
+                ofNullable(query.getRoleKey()).map(fn -> buildStringSpecification(fn, User_.roleKey)),
+                ofNullable(query.getActivated()).map(fn -> buildSpecification(fn, User_.activated))
+            )
             .filter(Optional::isPresent)
             .map(Optional::get)
             .reduce(Specification.where(
@@ -80,76 +80,90 @@ public class UserQueryService extends QueryService<User> {
     }
 
     private Specification<User> getLoginSpecificationForStrict(StringFilter loginFilter) {
-        Function<Root<User>, Join<User, UserLogin>> functionToEntity = userRoot -> (Join<User, UserLogin>) userRoot.fetch(User_.logins);
+        AtomicReference<CriteriaQuery<?>> queryReference = new AtomicReference<>();
+        Consumer<CriteriaQuery<?>> queryConsumer = queryReference::set;
+
+        Function<Root<User>, Join<User, UserLogin>> functionToEntity = userRoot -> {
+            if (queryReference.get().getResultType().equals(User.class)) {
+                return (Join<User, UserLogin>) userRoot.fetch(User_.logins);
+            } else {
+                return (Join<User, UserLogin>) userRoot.join(User_.logins);
+            }
+        };
         Function<Join<User, UserLogin>, Expression<String>> entityToColumn = entity -> entity.get(UserLogin_.login);
-        return buildSpecification(loginFilter, functionToEntity.andThen(entityToColumn));
+        return buildSpecification(loginFilter, functionToEntity.andThen(entityToColumn), queryConsumer);
     }
 
     private Specification<User> getLoginSpecificationForSoft(StringFilter loginFilter) {
+        Consumer<CriteriaQuery<?>> queryConsumer = query -> query.distinct(true);
+
         Function<Root<User>, Join<User, UserLogin>> functionToEntity = userRoot -> (Join<User, UserLogin>) userRoot.join(User_.logins);
         Function<Join<User, UserLogin>, Expression<String>> entityToColumn = entity -> entity.get(UserLogin_.login);
-        return buildSpecificationDistinct(loginFilter, functionToEntity.andThen(entityToColumn));
+        return buildSpecification(loginFilter, functionToEntity.andThen(entityToColumn), queryConsumer);
     }
 
-    protected Specification<User> buildSpecificationDistinct(StringFilter filter, Function<Root<User>, Expression<String>> metaclassFunction) {
+    protected Specification<User> buildSpecification(StringFilter filter,
+                                                     Function<Root<User>, Expression<String>> metaclassFunction,
+                                                     Consumer<CriteriaQuery<?>> queryConsumer) {
+
         if (filter.getEquals() != null) {
-            return equalsSpecificationDistinct(metaclassFunction, filter.getEquals());
+            return equalsSpecification(metaclassFunction, filter.getEquals(), queryConsumer);
         } else if (filter.getIn() != null) {
-            return valueInDistinct(metaclassFunction, filter.getIn());
+            return valueIn(metaclassFunction, filter.getIn(), queryConsumer);
         } else if (filter.getContains() != null) {
-            return likeUpperSpecificationDistinct(metaclassFunction, filter.getContains());
+            return likeUpperSpecification(metaclassFunction, filter.getContains(), queryConsumer);
         } else if (filter.getDoesNotContain() != null) {
-            return doesNotContainSpecificationDistinct(metaclassFunction, filter.getDoesNotContain());
+            return doesNotContainSpecification(metaclassFunction, filter.getDoesNotContain(), queryConsumer);
         } else if (filter.getNotEquals() != null) {
-            return notEqualsSpecificationDistinct(metaclassFunction, filter.getNotEquals());
+            return notEqualsSpecification(metaclassFunction, filter.getNotEquals(), queryConsumer);
         } else if (filter.getSpecified() != null) {
-            return byFieldSpecifiedDistinct(metaclassFunction, filter.getSpecified());
+            return byFieldSpecified(metaclassFunction, filter.getSpecified(), queryConsumer);
         }
         return null;
     }
 
-    protected <X> Specification<User> equalsSpecificationDistinct(Function<Root<User>, Expression<X>> metaclassFunction, X value) {
+    protected <X> Specification<User> equalsSpecification(Function<Root<User>, Expression<X>> metaclassFunction, X value, Consumer<CriteriaQuery<?>> queryConsumer) {
         return (root, query, builder) -> {
-            query.distinct(true);
+            queryConsumer.accept(query);
             return builder.equal(metaclassFunction.apply(root), value);
         };
     }
 
-    protected <X> Specification<User> notEqualsSpecificationDistinct(Function<Root<User>, Expression<X>> metaclassFunction, X value) {
+    protected <X> Specification<User> notEqualsSpecification(Function<Root<User>, Expression<X>> metaclassFunction, X value, Consumer<CriteriaQuery<?>> queryConsumer) {
         return (root, query, builder) -> {
-            query.distinct(true);
+            queryConsumer.accept(query);
             return builder.not(builder.equal(metaclassFunction.apply(root), value));
         };
     }
 
-    protected Specification<User> doesNotContainSpecificationDistinct(Function<Root<User>, Expression<String>> metaclassFunction, String value) {
+    protected Specification<User> doesNotContainSpecification(Function<Root<User>, Expression<String>> metaclassFunction, String value, Consumer<CriteriaQuery<?>> queryConsumer) {
         return (root, query, builder) -> {
-            query.distinct(true);
+            queryConsumer.accept(query);
             return builder.not(builder.like(builder.upper(metaclassFunction.apply(root)), wrapLikeQuery(value)));
         };
     }
 
-    protected Specification<User> likeUpperSpecificationDistinct(Function<Root<User>, Expression<String>> metaclassFunction, String value) {
+    protected Specification<User> likeUpperSpecification(Function<Root<User>, Expression<String>> metaclassFunction, String value, Consumer<CriteriaQuery<?>> queryConsumer) {
         return (root, query, builder) -> {
-            query.distinct(true);
+            queryConsumer.accept(query);
             return builder.like(builder.upper(metaclassFunction.apply(root)), wrapLikeQuery(value));
         };
     }
 
-    protected <X> Specification<User> byFieldSpecifiedDistinct(Function<Root<User>, Expression<X>> metaclassFunction, boolean specified) {
+    protected <X> Specification<User> byFieldSpecified(Function<Root<User>, Expression<X>> metaclassFunction, boolean specified, Consumer<CriteriaQuery<?>> queryConsumer) {
         return (root, query, builder) -> {
-            query.distinct(true);
+            queryConsumer.accept(query);
             return specified ? builder.isNotNull(metaclassFunction.apply(root)) : builder.isNull(metaclassFunction.apply(root));
         };
     }
 
-    protected <X> Specification<User> valueInDistinct(Function<Root<User>, Expression<X>> metaclassFunction, Collection<X> values) {
+    protected <X> Specification<User> valueIn(Function<Root<User>, Expression<X>> metaclassFunction, Collection<X> values, Consumer<CriteriaQuery<?>> queryConsumer) {
         return (root, query, builder) -> {
+            queryConsumer.accept(query);
             CriteriaBuilder.In<X> predicate = builder.in(metaclassFunction.apply(root));
             for (X value : values) {
                 predicate = predicate.value(value);
             }
-            query.distinct(true);
             return predicate;
         };
     }
