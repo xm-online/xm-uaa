2c8373f0319078dcef2a7b6a7d4cb479dde4597d
aroze <aroze@vodafone.ua>
2022-08-05 18:44:47 +0300

    fix integration tests for UserQueryService

--- src/test/java/com/icthh/xm/uaa/service/query/UserQueryServiceIntTest.java
+++ src/test/java/com/icthh/xm/uaa/service/query/UserQueryServiceIntTest.java
@@ -1,6 +1,5 @@
 package com.icthh.xm.uaa.service.query;
 
-import com.icthh.xm.commons.tenant.TenantContextHolder;
 import com.icthh.xm.uaa.UaaApp;
 import com.icthh.xm.uaa.config.xm.XmOverrideConfiguration;
 import com.icthh.xm.uaa.domain.User;
@@ -16,23 +15,16 @@ import io.github.jhipster.service.filter.StringFilter;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.transaction.AfterTransaction;
-import org.springframework.test.context.transaction.BeforeTransaction;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static com.icthh.xm.commons.tenant.TenantContextUtils.buildTenant;
-import static com.icthh.xm.uaa.UaaTestConstants.DEFAULT_TENANT_KEY_VALUE;
 import static java.lang.Boolean.TRUE;
 
 /**
@@ -45,10 +37,8 @@ import static java.lang.Boolean.TRUE;
     UaaApp.class,
     XmOverrideConfiguration.class
 })
-@Transactional
 public class UserQueryServiceIntTest {
 
-    private static final String ROLE_SUPER_ADMIN = "SUPER-ADMIN";
     private static final String ROLE_USER = "ROLE_USER";
 
     private static final String FIRST_USER_KEY = "strictFirst";
@@ -67,29 +57,15 @@ public class UserQueryServiceIntTest {
     private UserQueryService userQueryService;
 
     @Autowired
-    private TenantContextHolder tenantContextHolder;
-
-    @Autowired
     private TenantPropertiesService tenantPropertiesService;
-
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
 
     @Before
     public void before() {
         tenantPropertiesService.onInit("/config/tenants/XM/uaa/uaa.yml", "{}");
+
+        userRepository.deleteAll();
         createTestUser(FIRST_USER_KEY, FIRST_USER_LOGIN);
         createTestUser(SECOND_USER_KEY, SECOND_USER_LOGIN);
-    }
-
-    @BeforeTransaction
-    public void beforeTransaction() {
-        tenantContextHolder.getPrivilegedContext().setTenant(buildTenant(DEFAULT_TENANT_KEY_VALUE));
-    }
-
-    @AfterTransaction
-    public void afterTransaction() {
-        tenantContextHolder.getPrivilegedContext().destroyCurrentContext();
     }
 
     @Test
@@ -121,10 +97,10 @@ public class UserQueryServiceIntTest {
     @Test
     public void findAllUsersByStrictMatch_valueIn() {
 
-        StrictUserFilterQuery adminFilterQuery = new StrictUserFilterQuery();
-        adminFilterQuery.setLogin((StringFilter) new StringFilter().setIn(List.of(FIRST_USER_LOGIN, SECOND_USER_LOGIN)));
+        StrictUserFilterQuery filterQuery = new StrictUserFilterQuery();
+        filterQuery.setLogin((StringFilter) new StringFilter().setIn(List.of(FIRST_USER_LOGIN, SECOND_USER_LOGIN)));
 
-        Page<UserDTO> page = userQueryService.findAllUsersByStrictMatch(adminFilterQuery, PageRequest.of(0, 3));
+        Page<UserDTO> page = userQueryService.findAllUsersByStrictMatch(filterQuery, PageRequest.of(0, 3));
 
         Assert.assertEquals(2, page.getTotalElements());
         Assert.assertEquals(2, page.getContent().size());
@@ -134,7 +110,6 @@ public class UserQueryServiceIntTest {
     public void findAllUsersByStrictMatch_notContains() {
 
         StrictUserFilterQuery filterQuery = new StrictUserFilterQuery();
-        filterQuery.setRoleKey((StringFilter) new StringFilter().setNotEquals(ROLE_SUPER_ADMIN));
         filterQuery.setLogin(new StringFilter().setDoesNotContain("first"));
 
         Page<UserDTO> page = userQueryService.findAllUsersByStrictMatch(filterQuery, PageRequest.of(0, 3));
@@ -146,11 +121,10 @@ public class UserQueryServiceIntTest {
     @Test
     public void findAllUsersByStrictMatch_notEquals() {
 
-        StrictUserFilterQuery adminFilterQuery = new StrictUserFilterQuery();
-        adminFilterQuery.setRoleKey((StringFilter) new StringFilter().setNotEquals(ROLE_SUPER_ADMIN));
-        adminFilterQuery.setLogin((StringFilter) new StringFilter().setNotEquals(FIRST_USER_LOGIN));
+        StrictUserFilterQuery filterQuery = new StrictUserFilterQuery();
+        filterQuery.setLogin((StringFilter) new StringFilter().setNotEquals(FIRST_USER_LOGIN));
 
-        Page<UserDTO> page = userQueryService.findAllUsersByStrictMatch(adminFilterQuery, PageRequest.of(0, 3));
+        Page<UserDTO> page = userQueryService.findAllUsersByStrictMatch(filterQuery, PageRequest.of(0, 3));
 
         Assert.assertEquals(1, page.getTotalElements());
         Assert.assertEquals(1, page.getContent().size());
@@ -160,7 +134,6 @@ public class UserQueryServiceIntTest {
     public void findAllUsersByStrictMatch_specified() {
 
         StrictUserFilterQuery filterQuery = new StrictUserFilterQuery();
-        filterQuery.setRoleKey((StringFilter) new StringFilter().setNotEquals(ROLE_SUPER_ADMIN));
         filterQuery.setLogin((StringFilter) new StringFilter().setSpecified(TRUE));
 
         Page<UserDTO> page = userQueryService.findAllUsersByStrictMatch(filterQuery, PageRequest.of(0, 3));
@@ -177,8 +150,8 @@ public class UserQueryServiceIntTest {
 
         Page<UserDTO> page = userQueryService.findAllUsersByStrictMatch(filterQuery, PageRequest.of(0, 3));
 
-        Assert.assertEquals(3, page.getTotalElements());
-        Assert.assertEquals(3, page.getContent().size());
+        Assert.assertEquals(2, page.getTotalElements());
+        Assert.assertEquals(2, page.getContent().size());
     }
 
     @Test
@@ -193,7 +166,7 @@ public class UserQueryServiceIntTest {
         Assert.assertEquals(1, page.getContent().size());
     }
 
-    private User createTestUser(String userKey, String email) {
+    private void createTestUser(String userKey, String email) {
         UserLogin userLogin = new UserLogin();
         userLogin.setTypeKey(UserLoginType.EMAIL.getValue());
         userLogin.setLogin(email);
@@ -209,6 +182,6 @@ public class UserQueryServiceIntTest {
         user.setFirstName(FIRST_NAME);
         user.setLastName(LAST_NAME);
 
-        return userRepository.saveAndFlush(user);
+        userRepository.saveAndFlush(user);
     }
 }
