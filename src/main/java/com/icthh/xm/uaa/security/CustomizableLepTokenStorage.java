b20b269277f2b602b0f59e7942070477564db7f9
ypashkov <ypashkov@itsmartflex.com>
2022-08-13 00:54:05 +0300

    Add ability to read refresh token from LEP

--- src/main/java/com/icthh/xm/uaa/security/CustomizableLepTokenStorage.java
+++ src/main/java/com/icthh/xm/uaa/security/CustomizableLepTokenStorage.java
@@ -1,10 +1,13 @@
 package com.icthh.xm.uaa.security;
 
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 public interface CustomizableLepTokenStorage {
     OAuth2AccessToken readAccessToken(String tokenValue);
 
     OAuth2Authentication readAuthentication(OAuth2AccessToken token);
+
+    OAuth2RefreshToken readRefreshToken(String tokenValue);
 }
