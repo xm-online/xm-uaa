f77effd187c2eab75c70624944f378f13de5dba4
aroze <aroze@vodafone.ua>
2023-06-25 19:58:02 +0300

    [tryFilterByDataParam] fix specification

--- src/main/java/com/icthh/xm/uaa/service/query/UserQueryService.java
+++ src/main/java/com/icthh/xm/uaa/service/query/UserQueryService.java
@@ -109,9 +109,9 @@ public class UserQueryService extends QueryService<User> {
         List<Optional<Specification<User>>> specs = new ArrayList<>();
         for (Map.Entry<String, String> entry : dataAttributes.entrySet()) {
             Specification<User> spec = (root, query, cb) -> cb.equal(cb.function("JSON_VALUE", String.class,
-                    new HibernateInlineExpression(cb, "DATA"),
-                    new HibernateInlineExpression(cb, "'$." + entry.getKey() + "'")),
-                new LiteralExpression("'" + entry.getValue() + "'"));
+                    cb.literal("DATA"),
+                    cb.literal("'$." + entry.getKey() + "'")),
+                cb.literal("'" + entry.getValue() + "'"));
             specs.add(Optional.of(spec));
             log.info("Specification: {}", spec);
         }
