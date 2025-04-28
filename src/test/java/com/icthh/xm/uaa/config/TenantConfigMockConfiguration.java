3e0aef191e567dd8c112c9ca92337cfe73bea676
ssenko <serhii.senko@jevera.software>
2025-04-28 11:46:03 +0300

    fix tests

--- src/test/java/com/icthh/xm/uaa/config/TenantConfigMockConfiguration.java
+++ src/test/java/com/icthh/xm/uaa/config/TenantConfigMockConfiguration.java
@@ -4,12 +4,15 @@ import com.icthh.xm.commons.config.client.repository.CommonConfigRepository;
 import com.icthh.xm.commons.config.client.repository.TenantConfigRepository;
 import com.icthh.xm.commons.config.client.repository.TenantListRepository;
 import com.icthh.xm.commons.config.client.service.TenantConfigService;
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashSet;
 import java.util.Set;
+import org.springframework.core.env.Environment;
 
+import static java.util.Arrays.asList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
@@ -18,15 +21,13 @@ import static org.mockito.Mockito.when;
 @Configuration
 public class TenantConfigMockConfiguration {
 
-    private Set<String> tenants = new HashSet<>();
-
-    {
-        tenants.add("XM");
-        tenants.add("DEMO");
-    }
-
     @Bean
-    public TenantListRepository tenantListRepository() {
+    public TenantListRepository tenantListRepository(Environment env) {
+        Set<String> tenants = new HashSet<>();
+        tenants.add("XM");
+        if (!asList(env.getActiveProfiles()).contains("oracle-test")) {
+            tenants.add("DEMO");
+        }
         TenantListRepository mockTenantListRepository = mock(TenantListRepository.class);
         doAnswer(mvc -> tenants.add(mvc.getArguments()[0].toString())).when(mockTenantListRepository).addTenant(any());
         doAnswer(mvc -> tenants.remove(mvc.getArguments()[0].toString())).when(mockTenantListRepository).deleteTenant(any());
