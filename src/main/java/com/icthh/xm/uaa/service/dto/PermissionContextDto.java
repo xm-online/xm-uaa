41b3c376999d50a1e31faf83bb4f810ec9c15214
vosiievska <valentinaosievskaya@gmail.com>
2025-04-28 09:34:09 +0300

    DF-639 collect context permissions per service

--- src/main/java/com/icthh/xm/uaa/service/dto/PermissionContextDto.java
+++ src/main/java/com/icthh/xm/uaa/service/dto/PermissionContextDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -15,10 +16,25 @@ public class PermissionContextDto implements Serializable {
 
     private List<String> permissions;
     private Map<String, Object> ctx;
-    private String hash;
+    private Integer hash;
 
     public PermissionContextDto() {
         this.permissions = List.of();
         this.ctx = Map.of();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PermissionContextDto that = (PermissionContextDto) o;
+        return Objects.equals(permissions, that.permissions) && Objects.equals(ctx, that.ctx);
+    }
+
+    @Override
+    public int hashCode() {
+        if (hash == null) {
+            hash = Objects.hash(permissions, ctx);
+        }
+        return hash;
     }
 }
