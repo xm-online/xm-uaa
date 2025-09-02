6abb29f214976c7277b2f3fe2349b151664f95df
viktorpavlenko <vitek.pavfurious@gmail.com>
2025-09-02 18:40:17 +0300

    XM1-35852: Update version postgres;

--- src/test/java/com/icthh/xm/uaa/service/query/UserQueryServicePostgresIntTest.java
+++ src/test/java/com/icthh/xm/uaa/service/query/UserQueryServicePostgresIntTest.java
@@ -88,7 +88,7 @@ public class UserQueryServicePostgresIntTest {
     private static final Boolean THIRD_VALUE_BOOLEAN = true;
 
     @ClassRule
-    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:14.17")
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:17")
          .withDatabaseName("uaa")
          .withUsername("xm")
          .withPassword("xm");
