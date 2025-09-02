31a092e9ba711e5afc54c088260a9640886b000e
viktorpavlenko <vitek.pavfurious@gmail.com>
2025-09-02 15:00:40 +0300

    XM1-35852: Enable tests and update version postgres;

--- src/test/java/com/icthh/xm/uaa/service/query/UserQueryServicePostgresIntTest.java
+++ src/test/java/com/icthh/xm/uaa/service/query/UserQueryServicePostgresIntTest.java
@@ -88,7 +88,7 @@ public class UserQueryServicePostgresIntTest {
     private static final Boolean THIRD_VALUE_BOOLEAN = true;
 
     @ClassRule
-    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:12.7")
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:17")
         .withDatabaseName("uaa");
 
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
