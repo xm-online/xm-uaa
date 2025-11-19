d7923ac9138bc7503c8ac9c11adc07d84d7981c3
osiievska23 <valentinaosievskaya@gmail.com>
2025-11-19 09:30:05 +0200

    force user logout API

--- src/main/java/com/icthh/xm/uaa/service/UserService.java
+++ src/main/java/com/icthh/xm/uaa/service/UserService.java
@@ -38,7 +38,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
