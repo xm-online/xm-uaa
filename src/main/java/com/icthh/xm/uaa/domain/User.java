5a6b985e06959f846f0d3a51823e649823420f6d
ssenko <sergeysenja1992@gmail.com>
2026-01-29 22:17:54 +0200

    Fix override permissions when it in several ms with same key

--- src/main/java/com/icthh/xm/uaa/domain/User.java
+++ src/main/java/com/icthh/xm/uaa/domain/User.java
@@ -1,15 +1,11 @@
 package com.icthh.xm.uaa.domain;
 
-import static com.google.common.collect.Iterables.getFirst;
-import static java.util.Objects.requireNonNullElse;
-import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.icthh.xm.commons.migration.db.jsonb.Jsonb;
 import com.icthh.xm.uaa.domain.converter.MapToStringConverter;
 import com.icthh.xm.uaa.domain.converter.RoleKeyConverter;
-import com.icthh.xm.uaa.validator.JsonData;
 import com.icthh.xm.uaa.repository.converter.OtpChannelTypeAttributeConverter;
+import com.icthh.xm.uaa.validator.JsonData;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -38,6 +34,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
+import static com.google.common.collect.Iterables.getFirst;
+import static java.util.Objects.requireNonNullElse;
+import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
 
 /**
  * A user.
